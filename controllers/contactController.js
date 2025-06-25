14e3e1b927bad746da027f59dd8d37f266a5a396
fire <akshaysunilkumar2004@gmail.com>
2025-06-25 23:20:30 +0530

    Built the backend of a contact mangagement system

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,16 +4,16 @@ const Contact = require("../models/contactModel")
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const getContacts = asyncHandler(async(req,res) =>{
-    const contacts = await Contact.find()
+    const contacts = await Contact.find({user_id: req.user.id})
     res.status(200).json(contacts);
 });
 
 // @desc Get individual contacts
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 
 const getContact =  asyncHandler(async(req,res) =>{
     const contact = await Contact.findById(req.params.id)
@@ -27,7 +27,7 @@ const getContact =  asyncHandler(async(req,res) =>{
 
 //@desc create new contacts
 //@route Post /api/contacts
-//@access public
+//@access private
 
 const createContact = asyncHandler(async(req,res) =>{
     console.log("The requested body is",req.body)
@@ -41,13 +41,14 @@ const createContact = asyncHandler(async(req,res) =>{
         name,
         email,
         phone,
+        user_id:req.user.id
     })
     res.status(201).json(contact);
 });
 
 //@desc update contacts
 //@route Put /api/contacts 
-//@access public
+//@access private
 
 const updateContact = asyncHandler(async(req,res) =>{
 
@@ -56,6 +57,11 @@ const updateContact = asyncHandler(async(req,res) =>{
         res.status(404);
         throw new Error("Contact not Found")
 
+    }
+
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403)
+        throw new Error("User does not have permission to update")
     }
 
     const updatedContact = await Contact.findByIdAndUpdate(
@@ -68,7 +74,7 @@ const updateContact = asyncHandler(async(req,res) =>{
 
 //@desc delete contacts
 //@route delete /api/contacts
-//@access public
+//@access private
 
 const deleteContact = asyncHandler(async(req,res) =>{
         const contact = await Contact.findById(req.params.id)
@@ -76,6 +82,10 @@ const deleteContact = asyncHandler(async(req,res) =>{
         res.status(404);
         throw new Error("Contact not Found")
 
+    }
+        if(contact.user_id.toString() !== req.user.id){
+        res.status(403)
+        throw new Error("User does not have permission to update")
     }
 
         await Contact.findByIdAndDelete(req.params.id)
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler")
 /** @type {import("mongoose").Model} */
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 //@desc Register a user
 //@route GET /api/users/register
@@ -14,13 +16,30 @@ const registerUser = asyncHandler(async(req,res) => {
         throw new Error("All fields are mandatory")
     }
 
-    const userAvailable = await User.findOne(email);
+    const userAvailable = await User.findOne({email});
     if(userAvailable){ 
         res.status(400)
         throw new Error("User is already registered!")
     }
 
-    res.json({message:"Register the user"})
+    const hashedPassword = await bcrypt.hash(password,10)
+
+    console.log("hashed password:",hashedPassword)
+
+    const user = await User.create({
+        username,
+        email,
+        password:hashedPassword,
+    })
+
+    console.log("User created",user)
+    if(user){
+        res.status(201).json({_id:user.id, email:user.email})
+    }else{
+        res.status(400)
+        throw new Error("User data is not valid")
+    }
+    res.json({message:"Registered the user"})
 });
 
 
@@ -29,14 +48,39 @@ const registerUser = asyncHandler(async(req,res) => {
 //@access public
 
 const loginUser = asyncHandler(async(req,res) => {
-    res.json({message:"login the user"})
+    const {email,password} = req.body;
+    if(!email,!password){
+        res.status(400)
+        throw new Error("All fields are mandatory!")
+    }
+
+    const user = await User.findOne({email})
+
+
+
+    if(user&& (await bcrypt.compare(password,user.password))){
+        const accessToken = jwt.sign({
+            user:{
+                username:user.username,
+                email:user.email,
+                id:user.id,
+            },
+        },process.env.ACCESS_TOKEN_SECRET,
+         {expiresIn:"1h"}
+)
+        res.status(200).json({accessToken})
+    }else{
+        res.status(401)
+        throw new Error("Email or password not valid")
+    }
+
 });
 
 //@desc current user info
 //@route GET /api/users/current
 //@access private
 const currentUser = asyncHandler(async(req,res) => {
-    res.json({message:"current user information"})
+    res.json(req.user)
 });
 
 module.exports = {registerUser,loginUser,currentUser}
